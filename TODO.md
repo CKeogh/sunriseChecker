bbf667e97a7585cff45f2b3c0d069e36479fb67f
CKeogh <c.keogh.10.ck@gmail.com>
2019-02-11 20:32:45 +0000

    begin changing to openweathermap API

--- TODO.md
+++ TODO.md
@@ -1,9 +1,6 @@
 **TO DO**
 
 1. accept location from user
-2. find out if there's going to be a good sunset/sunrise
-    * determine location east of user location
-    * determine sunrise/sunset time
-    * determine if its light cloud/clear
-3. respond with yes or no
+2. check sunset 1 hour before due every day and alert if its going to be good
+3. check sunrise evening before and alert if its going to be good
 
--- index.js
+++ index.js
@@ -2,35 +2,51 @@
 
 const https = require('https');
 const fs = require('fs');
-const { getSunriseData, getSunsetData } = require('./functions.js')
+const { getSunriseData, getSunsetData } = require('./functions.js');
+
+fs.readFile('./city.list.json','utf8', (err, data) => {
+    const cities = JSON.parse(data);
+    //console.log(cities.filter(city => city.name === "Liverpool" && city.country === 'GB'));
+})
 
 const options = {
     hostname: "www.metaweather.com",
     path: "/api/location/26734/",
-
     method: "GET"
 }
 
-const req = https.request(options, (res) => {
+const apiKey = '29a6fa3d041a4d4365d30ba4adfbdf5c';
+const Liverpool = 2644210;
+
+const openWeather = {
+    hostname: "api.openweathermap.org",
+    path: `/data/2.5/forecast?id=${Liverpool}&APPID=${apiKey}`
+}
+
+const req = https.request(openWeather, (res) => {
     let body = "";
     res.on("data", (incomingData) => {
         body += incomingData.toString();
     });
     res.on("end", () => {
 
+
             const data = JSON.parse(body);
-            const sunriseData = getSunriseData(data);
-            const sunsetData = getSunsetData(data);
+            const sunriseWeather = data.list.filter(report => /06:00:00/.test(report.dt_txt));
+           // const sunriseWeather = data.list[1].dt_txt;
+            console.log(sunriseWeather);
+            // const sunriseData = getSunriseData(data);
+            // const sunsetData = getSunsetData(data);
     
-            if (sunriseData.isGood) {
-                console.log(`the next sunrise will be ${sunriseData.nextSunriseDay} at ${sunriseData.nextSunriseTime} and it's going to be a good one! `)
-            }
-            if (sunsetData.isGood) {
-                console.log(`the next sunset will be ${sunsetData.nextSunsetDay} at ${sunsetData.nextSunsetTime} and it's going to be a good one!`)
-            }
-            if (!sunsetData.isGood && !sunriseData.isGood) {
-                console.log(`I'm afraid there won't be much to see ${sunriseData.nextSunriseDay} but be sure to check back later`)
-            }
+            // if (sunriseData.isGood) {
+            //     console.log(`the next sunrise will be ${sunriseData.nextSunriseDay} at ${sunriseData.nextSunriseTime} and it's going to be a good one! `)
+            // }
+            // if (sunsetData.isGood) {
+            //     console.log(`the next sunset will be ${sunsetData.nextSunsetDay} at ${sunsetData.nextSunsetTime} and it's going to be a good one!`)
+            // }
+            // if (!sunsetData.isGood && !sunriseData.isGood) {
+            //     console.log(`I'm afraid there won't be much to see ${sunriseData.nextSunriseDay} but be sure to check back later`)
+            // }
     });
 });
 
